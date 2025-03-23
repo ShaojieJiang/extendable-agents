ff22ca433bc32d076a217662fcfcb7366b7e70d7
Shaojie Jiang <shaojie.jiang1@gmail.com>
2025-03-23 18:46:49 +0100

    Move core to aic-core

--- extendable_agents/app/main.py
+++ extendable_agents/app/main.py
@@ -1,8 +1,9 @@
 """Main page."""
 
 import streamlit as st
+from aic_core.agent_hub import AgentHub
+from extendable_agents.constants import HF_REPO_ID
 from extendable_agents.constants import PAGES
-from extendable_agents.hub import HFRepo
 from extendable_agents.logging import get_logger
 
 
@@ -12,7 +13,7 @@ logger = get_logger(__name__)
 @st.cache_resource
 def load_repo() -> None:
     """Load repo."""
-    hf_repo = HFRepo()
+    hf_repo = AgentHub(HF_REPO_ID)
     hf_repo.download_files()
     logger.info("Loaded repo")
 
