181d89eda66ac8b9f7ca8b560667cbbf31dde20d
Shaojie Jiang <shaojie.jiang1@gmail.com>
2025-03-26 07:39:16 +0100

    Implement file-redownload

--- extendable_agents/app/agent.py
+++ extendable_agents/app/agent.py
@@ -1,7 +1,16 @@
 """Custom Agent."""
 
+from aic_core.agent.agent_hub import AgentHub
 from aic_core.streamlit.agent_config import AgentConfigPage
 from extendable_agents.constants import HF_REPO_ID
 
 
-AgentConfigPage(HF_REPO_ID).run()
+class EAConfigPage(AgentConfigPage):
+    """Agent Config Page."""
+
+    def re_download_files(self) -> None:
+        """Re-download files to local."""
+        AgentHub(self.repo_id).download_files()
+
+
+EAConfigPage(HF_REPO_ID).run()
