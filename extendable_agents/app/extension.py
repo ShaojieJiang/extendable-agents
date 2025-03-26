add570a0db5a96dbf9c1f42c63b36466d704dd90
Shaojie Jiang <shaojiejiang@Shaojies-Mini.lan>
2025-03-26 17:34:13 +0100

    Adapt to latest core

--- extendable_agents/app/extension.py
+++ extendable_agents/app/extension.py
@@ -1,7 +1,16 @@
 """Extension page."""
 
+from aic_core.agent.agent_hub import AgentHub
 from aic_core.streamlit.tool_config import ToolConfigPage
 from extendable_agents.constants import HF_REPO_ID
 
 
-ToolConfigPage(HF_REPO_ID).run()
+class EAToolConfigPage(ToolConfigPage):
+    """Tool Config Page."""
+
+    def re_download_files(self) -> None:
+        """Re-download files to local."""
+        AgentHub(self.repo_id).download_files()
+
+
+EAToolConfigPage(HF_REPO_ID).run()
