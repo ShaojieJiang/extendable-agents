b18fbc6e409872331e696635daccbf36eafe6eba
Shaojie Jiang <shaojie.jiang1@gmail.com>
2025-03-27 18:55:18 +0100

    Fix page state

--- extendable_agents/app/chatbot.py
+++ extendable_agents/app/chatbot.py
@@ -5,12 +5,30 @@ import streamlit as st
 from aic_core.agent.agent import AgentConfig
 from aic_core.agent.agent import AgentFactory
 from aic_core.streamlit.agent_page import AgentPage
+from aic_core.streamlit.page import app_state
 from pydantic_ai import Agent
+from pydantic_ai.messages import ModelMessage
 from extendable_agents.constants import HF_REPO_ID
+
+
+@app_state(__file__)
+class PageState:
+    """Page state."""
+
+    chat_history: list[ModelMessage] = []
 
 
 class ChatbotPage(AgentPage):
     """Chatbot page."""
+
+    def __init__(self, repo_id: str, page_title: str) -> None:
+        """Initialize the page."""
+        super().__init__()
+        self.app_state = PageState()
+        self.repo_id = repo_id
+        self.page_title = page_title
+        self.user_role = "user"
+        self.assistant_role = "assistant"
 
     def get_agent(self, agent_name: str) -> Agent:
         """Get agent."""
