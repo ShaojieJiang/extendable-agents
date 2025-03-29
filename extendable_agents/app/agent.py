fd92c961f943e8c64d89a87559e8473a30791094
Shaojie Jiang <shaojie.jiang1@gmail.com>
2025-03-29 18:38:42 +0100

    Simplify config pages

--- extendable_agents/app/agent.py
+++ extendable_agents/app/agent.py
@@ -1,16 +1,7 @@
 """Custom Agent."""
 
-from aic_core.agent.agent_hub import AgentHub
 from aic_core.streamlit.agent_config import AgentConfigPage
 from extendable_agents.constants import HF_REPO_ID
 
 
-class EAConfigPage(AgentConfigPage):
-    """Agent Config Page."""
-
-    def re_download_files(self) -> None:
-        """Re-download files to local."""
-        AgentHub(self.repo_id).download_files()
-
-
-EAConfigPage(HF_REPO_ID).run()
+AgentConfigPage(HF_REPO_ID).run()
